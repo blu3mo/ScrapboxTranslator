39115e93a71447acc3c75ef13c56fa1e6ec68303
Bluemo <shutaro.aoyama@gmail.com>
2023-03-29 11:46:32 -0400

    Kinda-async

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import json
+import asyncio
+import aiohttp
 import openai
 
 MAX_TOKENS = 8000
@@ -14,19 +16,26 @@ Keep the number of lines and newlines. Never remove spaces at the beginning of e
 Brackets of [text] and [text.icon] must be kept. The content inside the bracket must never be changed.
 """
 
-def translate(text, role="user"):
-    print(text)
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {"role": "system", "content": PROMPT},
-            {"role": "user", "content": text}
-        ]
-    )
-    print(response.choices[0].message.content)
-    return response.choices[0].message.content
+async def async_translate(session, text, role="user"):
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {openai.api_key}"
+    }
 
-def translate_titles(title_list):
+    data = {
+        "model": "gpt-3.5-turbo",
+        "messages": [
+            {"role": "system", "content": PROMPT},
+            {"role": role, "content": text}
+        ]
+    }
+
+    async with session.post("https://api.openai.com/v1/chat/completions", headers=headers, json=data) as resp:
+        response = await resp.json()
+        print(response)
+        return response["choices"][0]["message"]["content"]
+
+async def translate_titles(session, title_list):
     translated_titles = []
     title_chunk = ""
 
@@ -34,26 +43,28 @@ def translate_titles(title_list):
         if len(title_chunk) + len(title) + 1 < MAX_TOKENS:
             title_chunk += title + "\n"
         else:
-            translated_chunk = translate(title_chunk, role="title_translation")
+            translated_chunk = await async_translate(session, title_chunk)
             translated_titles.extend(translated_chunk.split("\n")[:-1])
             title_chunk = title + "\n"
 
     if title_chunk:
-        translated_chunk = translate(title_chunk, role="title_translation")
+        translated_chunk = await async_translate(session, title_chunk)
         translated_titles.extend(translated_chunk.split("\n")[:-1])
 
     return translated_titles
 
-def translate_page(page_text):
+async def translate_page(session, page_text):
     if len(page_text) <= MAX_TOKENS:
-        return translate(page_text, role="page_translation")
+        return await async_translate(session, page_text)
     else:
         split_point = page_text.rfind("\n", 0, MAX_TOKENS)
         first_half = page_text[:split_point]
         second_half = page_text[split_point + 1:]
-        return translate_page(first_half) + "\n" + translate_page(second_half)
+        first_half_translated = await async_translate(session, first_half)
+        second_half_translated = await translate_page(session, second_half)
+        return first_half_translated + "\n" + second_half_translated
 
-def translate_json_file(input_file, output_file):
+async def translate_json_file(input_file, output_file):
     with open(input_file, 'r', encoding='utf-8') as f:
         data = json.load(f)
 
@@ -61,31 +72,30 @@ def translate_json_file(input_file, output_file):
 
     # Translate page titles
     title_list = [page['title'] for page in data['pages']]
-    print(title_list)
-    translated_titles = translate_titles(title_list)
-    print("title translation done");
 
-    for original_title, translated_title in zip(title_list, translated_titles):
-        title_translation_dict[original_title] = translated_title
+    async with aiohttp.ClientSession() as session:
+        translated_titles = await translate_titles(session, title_list)
 
-    print(translated_titles)
+        for original_title, translated_title in zip(title_list, translated_titles):
+            title_translation_dict[original_title] = translated_title
 
-    for page, translated_title in zip(data['pages'], translated_titles):
-        page['title'] = translated_title
+        for page, translated_title in zip(data['pages'], translated_titles):
+            page['title'] = translated_title
 
+        # Translate lines with translated titles replaced
+        for page in data['pages']:
+            page_text = "\n".join(page['lines'])
 
-    # Translate lines with translated titles replaced
-    for page in data['pages']:
-        page_text = "\n".join(page['lines'])
-
-        for jp_title, en_title in title_translation_dict.items():
-            page_text = page_text.replace(f"[{jp_title}]", f"[{en_title}]")
-
-        translated_text = translate_page(page_text)
-        page['lines'] = translated_text.split("\n")
+            for jp_title, en_title in title_translation_dict.items():
+                page_text = page_text.replace(f"[{jp_title}]", f"[{en_title}]")
+            translated_text = await translate_page(session, page_text)
+            page['lines'] = translated_text.split("\n")
 
     with open(output_file, 'w', encoding='utf-8') as f:
         json.dump(data, f, ensure_ascii=False, indent=2)
 
-print("start")
-translate_json_file(INPUT_PATH, OUTPUT_PATH)
+# Replace 'input.json' and 'output.json' with your desired file paths
+async def main():
+    await translate_json_file(INPUT_PATH, OUTPUT_PATH)
+
+asyncio.run(main())
