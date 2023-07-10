61133f37c2bf7eae328c9be6e4577113bb3aa218
Bluemo <shutaro.aoyama@gmail.com>
2023-07-10 15:32:27 +0700

    Retry when failed

--- translatePages.py
+++ translatePages.py
@@ -41,60 +41,66 @@ translation_end_count = 1
 # Set OpenAI API Key
 openai.api_key = os.environ.get("OPENAI_API_KEY")
 
-async def fetch_page_translation(pageId, page):
-
+async def fetch_page_translation(pageId, page, max_retries=3):
     """
     Fetches the translation of a page from OpenAI API
 
     Args:
         pageId (str): The pageId of the page
         page (str): The page to be translated
+        max_retries (int): The maximum number of retries in case of failure
 
     Returns:
         (str, str): A tuple of the pageId and the translated page
     """
+    temperature = 0
 
-    token_count = num_tokens_from_string(page)
-    model = ""
-    if token_count <= 1800:
-        model = "gpt-3.5-turbo"
-    elif 1800 < token_count <= 7000:
-        model = "gpt-3.5-turbo-16k"
-    else:
-        model = "gpt-3.5-turbo-16k"
-        page_token_count = num_tokens_from_string(page)
-        cutoff_len = int(len(page) * (7000 / page_token_count))
-        print("cutoff: " + str(cutoff_len))
-        page = page[:cutoff_len]
-    
-    max_tokens = MAX_TOKENS[model]
+    for attempt in range(max_retries):
+        token_count = num_tokens_from_string(page)
+        model = ""
+        if token_count <= 1800:
+            model = "gpt-3.5-turbo"
+        elif 1800 < token_count <= 7000:
+            model = "gpt-3.5-turbo-16k"
+        else:
+            model = "gpt-3.5-turbo-16k"
+            page_token_count = num_tokens_from_string(page)
+            cutoff_len = int(len(page) * (7000 / page_token_count))
+            print("cutoff: " + str(cutoff_len))
+            page = page[:cutoff_len]
+        
+        max_tokens = MAX_TOKENS[model]
 
-    try:
-        global translation_start_count
-        print("(#" + str(translation_start_count) + ") Starting Page Translation: " + page[:25].replace('\n', ' ') + "...")
-        translation_start_count += 1
-        response = await openai.ChatCompletion.acreate(
-            model=model,
-            messages=[
-                {
-                    "role": "system",
-                    "content": generate_system_prompt(get_links(page))
-                },
-                {
-                    "role": "user",
-                    "content": page
-                }
-            ],
-            temperature=0,
-            max_tokens=max_tokens
-        )
-        translated_page = response['choices'][0]['message']['content']
-        global translation_end_count
-        print("(#" + str(translation_end_count) + ") Finished Page Translation: " + page[:25].replace('\n', ' ') + "...")
-        translation_end_count += 1
+        try:
+            global translation_start_count
+            print("(#" + str(translation_start_count) + ") Starting Page Translation: " + page[:25].replace('\n', ' ') + "...")
+            translation_start_count += 1
+            response = await openai.ChatCompletion.acreate(
+                model=model,
+                messages=[
+                    {
+                        "role": "system",
+                        "content": generate_system_prompt(get_links(page))
+                    },
+                    {
+                        "role": "user",
+                        "content": page
+                    }
+                ],
+                temperature=temperature,
+                max_tokens=max_tokens
+            )
+            translated_page = response['choices'][0]['message']['content']
+            global translation_end_count
+            print("(#" + str(translation_end_count) + ") Finished Page Translation: " + page[:25].replace('\n', ' ') + "...")
+            translation_end_count += 1
 
-        return (pageId, translated_page)
-    except Exception as e:
-        print("Error in fetch_page_translation: " + page[:25].replace('\n', ' ') + "...")
-        print(e)
-        return (pageId, page)
+            return (pageId, translated_page)
+        except Exception as e:
+            print(f"Error in fetch_page_translation, attempt #{attempt + 1}: " + page[:25].replace('\n', ' ') + "...")
+            print(e)
+            if attempt == max_retries - 1:  # If this was the last attempt
+                return (pageId, page)
+            else:
+                print("Retrying...")
+                temperature += 0.5
